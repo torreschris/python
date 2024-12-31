c6e7e86f507b72285c3a31fc621cd3c9ace95a86
Christian Torres <christiantorres@Christians-MacBook-Air.local>
2024-12-30 23:01:27 -0800

    Adding login pages

---
+++ kanji/pages/Login.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import hashlib
+import os
+
+st.title("Login")
+
+if "username" not in st.session_state:
+    st.session_state.username = "Guest"
+
+st.sidebar.header('Logged in as:')
+emptysidebar = st.sidebar.empty()
+emptysidebar.markdown(f'<p style="font-family:Verdana; font-size:16px; color:green;">{st.session_state.username}</p>', unsafe_allow_html=True)
+
+username = st.text_input(label='Username:')
+password = st.text_input(label='Password:',type='password')
+
+# Input string
+input_string = username + password
+
+# Create an MD5 checksum
+checksum = hashlib.md5(input_string.encode()).hexdigest() + ".csv"
+
+#st.write("MD5 Checksum: " +  checksum)
+
+if st.button('login'):
+    if os.path.isfile(f'users/{checksum}'):
+        st.success('Succesfully logged in!')
+        st.session_state.username = username
+        st.balloons()
+        emptysidebar.markdown(f'<p style="font-family:Verdana; font-size:16px; color:green;">{st.session_state.username}</p>', unsafe_allow_html=True)
+
+    else:
+        st.error('Username and password is incorrect, try again.')
+
---
+++ kanji/pages/New_User.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import hashlib
+import os
+import shutil
+
+# Initialize session state for the button
+if 'button_clicked' not in st.session_state:
+    st.session_state.button_clicked = False
+
+if "username" not in st.session_state:
+    st.session_state.username = "Guest"
+
+st.title("New user account")
+
+# Styled text
+st.sidebar.header('Logged in as:')
+st.sidebar.markdown(f'<p style="font-family:Verdana; font-size:16px; color:green;">{st.session_state.username}</p>', unsafe_allow_html=True)
+
+
+username = st.text_input(label='Username:')
+password = st.text_input(label='Password:',type='password')
+
+# Input string
+input_string = username + password
+# Create an MD5 checksum
+checksum = hashlib.md5(input_string.encode()).hexdigest() + ".csv"
+
+if st.button('Create new account', disabled=st.session_state.button_clicked):
+    if not password:
+        st.error('Password cannot be blank.')
+    elif not os.path.isfile(f'users/{checksum}'):
+        st.success('New account successfully created!')
+        st.session_state['username'] = username
+        st.session_state['csvfile'] = checksum
+        shutil.copy('allkanji.csv', f'users/{checksum}')
+        st.session_state.button_clicked = True
+    else:
+        st.error('Username and password is already in use, please try again.')
+    
